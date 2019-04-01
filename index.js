b43d06df13064ec6b04e368171aaab16c4fc710c
Koray Dundar <koraydundar_40@hotmail.com>
2019-04-01 07:05:10 +0300

    High order function, mini calculator challenge is done

--- index.js
+++ index.js
@@ -1,10 +1,41 @@
-function buttonW () {
-    alert('Hey I am button W and I just got clicked')
+function button () {
+    alert('Hey I am button and I just got clicked')
 }
 
-document.querySelectorAll('button')[0].addEventListener('click', buttonW)
+// document.querySelectorAll('button')[0].addEventListener('click', buttonW)
 
 //same as above but anonymous function
-document.querySelectorAll('button')[1].addEventListener('click', function(){
-    alert('button a ')
-})
+// document.querySelectorAll('button')[1].addEventListener('click', function(){
+//     alert('button a ')
+// })
+
+
+let buttons = document.querySelectorAll('button')
+
+for (let i = 0; i < buttons.length; i++ ) {
+    document.querySelectorAll('button')[i].addEventListener('click', button)
+}
+
+
+// //High order functions - Mini calculator challenge
+// function add(num1, num2) {
+//     return num1 + num2;
+//     }
+     
+//     function subtract(num1, num2) {
+//     return num1 - num2;
+//     }
+     
+//     function multiply(num1, num2) {
+//     return num1 * num2;
+//     }
+     
+//     function divide(num1, num2) {
+//     return num1 / num2;
+//     }
+     
+//     function calculator(num1, num2, operator) {
+//     return operator(num1, num2);
+//     }
+
+//     console.log(calculator(3, 4, multiply))
