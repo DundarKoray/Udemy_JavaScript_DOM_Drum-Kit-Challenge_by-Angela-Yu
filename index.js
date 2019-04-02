0e37273de70ec66a6c6ae502db7e618113bf59e6
Koray Dundar <koraydundar_40@hotmail.com>
2019-04-02 18:31:55 +0300

    sound fixes

--- index.js
+++ index.js
@@ -24,37 +24,38 @@ for (let i = 0; i < buttons.length; i++ ) {
         console.log(buttonInnerHTML)
         switch (buttonInnerHTML) {
             case 'w':
-                let audio = new Audio('./sounds/tom-1.mp3')
-                audio.play()
+                let audioButtonW = new Audio('./sounds/tom-1.mp3')
+                audioButtonW.play()
             break;
 
             case 'a':
-                let audio = new Audio('./sounds/tom-2.mp3')
-                audio.play()
+                let audioButtonA = new Audio('./sounds/tom-2.mp3')
+                audioButtonA.play()
             break;
 
             case 's':
-                let audio = new Audio('./sounds/tom-3.mp3')
-                audio.play()
+                let audioButtonS = new Audio('./sounds/tom-3.mp3')
+                audioButtonS.play()
             break;
 
             case 'd':
-                let audio = new Audio('./sounds/tom-4.mp3')
-                audio.play()
+                let audioButtonD = new Audio('./sounds/tom-4.mp3')
+                audioButtonD.play()
             break;
             
             case 'j':
-                let audio = new Audio('./sounds/crash.mp3.mp3')
-                audio.play()
-                
+                let audioButtonJ = new Audio('./sounds/snare.mp3')
+                audioButtonJ.play()
+            break;
+
             case 'k':
-                let audio = new Audio('./sounds/kick-bass.mp3.mp3')
-                audio.play()
+                let audioButtonK = new Audio('./sounds/kick-bass.mp3')
+                audioButtonK.play()
             break;
             
             case 'l':
-                let audio = new Audio('./sounds/snare.mp3.mp3')
-                audio.play()
+                let audioButtonL = new Audio('./sounds/crash.mp3')
+                audioButtonL.play()
             break;
         }
     })
